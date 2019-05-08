9f48026024e170fa392e67b81167b12a50ddee49
Paulo Kauscher Pinto <2229378+pkpinto@users.noreply.github.com>
2019-05-08 23:15:19 +0100

    Issue #2, Split testing of api_key for a good and an bad api_key

--- asyncnewsapi/tests/test_session.py
+++ asyncnewsapi/tests/test_session.py
@@ -14,10 +14,14 @@ class SessionTest(TestCase):
             self.assertTrue(isinstance(api.loop, asyncio.AbstractEventLoop))
 
     @async_test
-    async def test_api_key(self):
+    async def test_api_key_correct(self):
         async with Session(api_key=APIKEY) as api:
             tasks = [api.top_headlines(language='en'), ]
-            try:
+            await asyncio.gather(*tasks)
+
+    @async_test
+    async def test_api_key_incorrect(self):
+        async with Session(api_key='1' * len(APIKEY)) as api:
+            tasks = [api.top_headlines(language='en'), ]
+            with self.assertRaises(aiohttp.client_exceptions.ClientResponseError):
                 await asyncio.gather(*tasks)
-            except aiohttp.client_exceptions.ClientResponseError as e:
-                raise e
