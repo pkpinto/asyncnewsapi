741fef6563bbae606cf15710c5dd265a8ce1e25d
Paulo Kauscher Pinto <2229378+pkpinto@users.noreply.github.com>
2019-05-14 22:53:20 +0100

    Add API key (encrypted with git-crypt)

--- asyncnewsapi/tests/helper.py
+++ asyncnewsapi/tests/helper.py
@@ -3,10 +3,10 @@ import sys
 
 
 try:
-    with open('test.api.key') as f:
+    with open('api.key') as f:
         APIKEY = f.read()
 except FileNotFoundError as e:
-    print('test.api.key file containing API key is missing from root of repo. '
+    print('api.key file containing API key is missing from root of repo. '
           'Go to https://newsapi.org to create a free API key.', file=sys.stderr)
     sys.exit()
 
