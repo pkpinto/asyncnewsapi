3b094d4700e4c60c6ea5b6de86a8e6b5e8772436
Paulo Kauscher Pinto <2229378+pkpinto@users.noreply.github.com>
2019-10-13 13:49:45 +0200

    The API key is stored as an env variable, no need to encrypt local files

--- asyncnewsapi/tests/helper.py
+++ asyncnewsapi/tests/helper.py
@@ -1,16 +1,11 @@
 import asyncio
+import os
 import sys
 
 
-try:
-    with open('api.key') as f:
-        APIKEY = f.read()
-except FileNotFoundError as e:
-    print('api.key file containing API key is missing from root of repo. '
-          'Go to https://newsapi.org to create a free API key.', file=sys.stderr)
-    sys.exit()
-except UnicodeDecodeError as e:
-    print('api.key is currently encrypted, unlock with git-crypt or replace it with a new key. '
+APIKEY = os.environ.get('NEWSAPI_KEY')
+if not APIKEY:
+    print('The NEWSAPI_KEY environment variable is not set, it should contain the API key. '
           'Go to https://newsapi.org to create a free API key.', file=sys.stderr)
     sys.exit()
 
