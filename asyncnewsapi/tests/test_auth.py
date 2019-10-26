16b69f4800b86579403aa031daaee5105c06c388
Paulo Kauscher Pinto <2229378+pkpinto@users.noreply.github.com>
2019-10-26 23:46:57 +0200

    Increase code coverage of auth (#2)

--- asyncnewsapi/tests/test_auth.py
+++ asyncnewsapi/tests/test_auth.py
@@ -15,13 +15,31 @@ def test_env_variable_api_key(monkeypatch):
 
 class TestAuth(object):
 
-    def test_keyauth_from_url(self):
-        keyauth = KeyAuth(api_key='test_api_key')
-        assert(keyauth.from_url(URL('https://newsapi.org/v2/top-headlines?email=john@email.com&apiKey=test_api_key')) == keyauth)
+    def test_api_key_not_none(self):
+        with pytest.raises(ValueError):
+            KeyAuth(None)
 
-    def test_keyauth_decode(self):
+    def test_from_url(self):
+        assert(KeyAuth.from_url(URL('https://newsapi.org/v2/top-headlines?email=john@email.com&apiKey=test_api_key')) == KeyAuth(api_key='test_api_key'))
+
+    def test_from_url_uses_yarl(self):
+        '''from_url takes url as a yarnl.URL object"'''
+        with pytest.raises(TypeError):
+            KeyAuth.from_url('https://newsapi.org/v2/top-headlines?email=john@email.com&apiKey=test_api_key')
+
+    def test_decode(self):
         assert(KeyAuth.decode('Basic test_api_key').encode() == 'test_api_key')
 
-    def test_keyauth_encode(self):
+    def test_decode_cannot_parse(self):
+        '''Authorization HTTP header should look like "Basic test_api_key"'''
+        with pytest.raises(ValueError):
+            KeyAuth.decode('test_api_key')
+
+    def test_decode_not_basic_auth(self):
+        '''Authorization HTTP header should look like "Basic test_api_key"'''
+        with pytest.raises(ValueError):
+            KeyAuth.decode('Not basic test_api_key')
+
+    def test_encode(self):
         keyauth = KeyAuth(api_key='test_api_key')
         assert(keyauth.encode() == 'test_api_key')
